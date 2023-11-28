375ea56981568308fdd014945a8459f3cd1b1a2d
arpank6 <arpank6@gmail.com>
2023-11-28 11:53:41 +0530

    Bixi @nd Push

--- src/test/java/mvnPracticePackage/ArpanBixiClass.java
+++ src/test/java/mvnPracticePackage/ArpanBixiClass.java
@@ -4,5 +4,9 @@ public class ArpanBixiClass {
 	public void arpan1() {
 		System.out.println("BIXI-MY First branch 1st Push");
 	}
+	
+	public void arpan2() {
+		System.out.println("BIXI-MY First branch 2nd Push");
+	}
 
 }
