93a3ff23834fa3e8df7e8b79b0dd267dac1153ed
rerishi <rishav.rq@gmail.com>
2023-11-28 11:45:08 +0530

    First Push by Rioshav

---
+++ src/test/java/mvnPracticePackage/Practice2_Rishav.java
@@ -0,0 +1,12 @@
+package mvnPracticePackage;
+
+import org.testng.annotations.Test;
+
+public class Practice2_Rishav {
+
+	@Test
+	public void helloWorld()
+	{
+		System.out.println("Hello World");
+	}
+}
